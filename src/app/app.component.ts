623d0378fd6d1dacfba2600f63efdc2c729a275c
LearsacaZ <learsacaz@hotmail.com>
2022-03-07 15:59:47 -0500

    date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DATE';
+  name = 'DATE';
+  public maxDate:any;
+  public dateNow:string = '0000-00-00';
+ 
+  constructor() {
+    this.maxDate = Date.now();
+    let dateObj = new Date();
+    let month = dateObj.getMonth() + 1; //months from 1-12
+    let day = dateObj.getDate();
+    let year = dateObj.getFullYear();
+    this.maxDate = `${year}-0${month}-0${day}`;
+    console.log(this.maxDate);
+  }
+  
+  public catchCalendarValue(ev: any) {
+    this.dateNow=ev.target.value;
+    console.log(ev.target.value);
+  }
 }
